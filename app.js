96a3c79f89fbc47cff083d7e29812610f93a17bb
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-24 13:08:05 +0000

    Begin configuration of app.js file

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res)=> {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log("Server started.")
+});
