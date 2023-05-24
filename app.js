74c7388fe33f247cb954d2006656f40681b7a635
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-24 13:39:40 +0000

    Start adding components to html file

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+
 app.use(express.static(__dirname + "/public"));
+
 
 app.get("/", (req, res)=> {
     res.sendFile(__dirname + "/index.html");
