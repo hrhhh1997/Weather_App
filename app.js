aece8ac84b96670eeeb008131cde709f644bcb44
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-25 15:24:20 +0000

    Give basic full functionality

--- app.js
+++ app.js
@@ -3,12 +3,43 @@ const express = require('express');
 const app = express();
 
 const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+const unit = 'units=metric';
+const apiKey = '&appID=' + process.env.API_KEY + '&';
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: true}));
 
 
 app.get("/", (req, res)=> {
-    res.sendFile(__dirname + "/index.html");
+    res.render("weatherinfo", {dataLocation: "", description: "", temperature: ""});
+});
+
+app.post("/", (req, res)=> {
+    let userInput = req.body.location;
+    userInput = userInput.trim();
+    if (userInput.length === 0) {
+        res.redirect("/");
+    } else {
+        async function getWeatherData() {
+            let finalUrl = apiEndpoint + unit + apiKey + 'q=' + userInput;
+
+            const response = await fetch(finalUrl, {mode: 'cors'});
+
+            if (response.status == 404) {
+                res.render("weatherinfo", {dataLocation: "Error: "+404, temperature: "Message: " +"City Not Found!", description: "Enter valid City name. Name entered: " + userInput});
+            } else {
+                const responseData = await response.json();
+                const temp = responseData['main']['temp'];
+                const description = responseData['weather'][0]['description'];
+                const dLocation = responseData['name'];
+
+                res.render("weatherinfo", {temperature: "Temperature: "+temp, description: "Description: "+description, dataLocation: "Location: "+dLocation});
+            }
+        }
+
+        getWeatherData();
+    }
 });
 
 
---
+++ public/js-files/index.js
@@ -0,0 +1,11 @@
+// const locationTextBox = document.querySelector("#locationTxt");
+// const searchButton = document.querySelector("button");
+
+// searchButton.addEventListener('click', ()=> {
+//     let locValue = (locationTextBox.value).trim();
+//     if (locValue.length === 0) {
+//         locationTextBox.style.boxShadow = '0 0 5px 5px red';
+//     } else {
+//         return;
+//     }
+// });
